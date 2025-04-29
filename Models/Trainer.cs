97370b3f8b9f4d26fbac12b37ada965390182a36
Mikkel Barfod <mib010@edu.zealand.dk>
2025-04-29 13:00:50 +0200

    Add Trainer, Add TrainerDB

---
+++ Models/Trainer.cs
@@ -0,0 +1,8 @@
+﻿using Gadevang_Tennis_Klub.Interfaces.Models;
+
+namespace Gadevang_Tennis_Klub.Models
+{
+    public class Trainer : ITrainer
+    {
+    }
+}
---
+++ Services/TrainerDB.cs
@@ -0,0 +1,7 @@
+﻿using Gadevang_Tennis_Klub.Interfaces.Services;
+namespace Gadevang_Tennis_Klub.Services
+{
+    public class TrainerDB : ITrainerDB
+    {
+    }
+}
