2c0a8ffddbe98b37c62613a62a363c10757c61f8
Mikkel Barfod <mib010@edu.zealand.dk>
2025-04-29 13:03:30 +0200

    add secret

---
+++ Services/Secret.cs
@@ -0,0 +1,15 @@
+﻿namespace Gadevang_Tennis_Klub.Services
+{
+    public class Secret
+    {
+
+        //private static string _connectionString = 
+        //public static string ConnectionString
+        //{
+        //    get
+        //    {
+        //        return _connectionString;
+        //    }
+        //}
+    }
+}
