3f112304f8ad24531d8a91d23a0e5afaedaa8516
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-04-30 09:55:23 +0200

    Adding to Event, Acitivty & EventBooking interfaces

--- Interfaces/Models/IActivity.cs
+++ Interfaces/Models/IActivity.cs
@@ -2,5 +2,10 @@
 {
     public interface IActivity
     {
+        int ID { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        DateTime Start { get; set; }
+        TimeOnly End { get; set; }
     }
 }
--- Interfaces/Services/IEventBookingDB.cs
+++ Interfaces/Services/IEventBookingDB.cs
@@ -1,6 +1,13 @@
-﻿namespace Gadevang_Tennis_Klub.Interfaces.Services
+﻿using Gadevang_Tennis_Klub.Interfaces.Models;
+
+namespace Gadevang_Tennis_Klub.Interfaces.Services
 {
     public interface IEventBookingDB
     {
+        List<IEventBooking> GetAllEventBookingsAsync();
+        bool CreateEventBookingAsync(IEventBooking eventBooking);
+        bool DeleteEventBookingAsync(int eventBookingID);
+        bool UpdateEventBookingAsync(IEventBooking eventBooking);
+        IEventBooking GetEventBookingById(int eventBookingID);
     }
 }
