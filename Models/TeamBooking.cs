cb23e153e8e28403fe72c9b5fe85bb9064f2a361
Malthe Anders Bruhn <mab017@edu.zealand.dk>
2025-04-30 10:51:49 +0200

    Fixed constructor for Team and TeamBooking

--- Models/TeamBooking.cs
+++ Models/TeamBooking.cs
@@ -4,9 +4,9 @@ namespace Gadevang_Tennis_Klub.Models
 {
     public class TeamBooking : ITeamBooking
     {
-        public int Member_ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Team_ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Member_ID { get; set; }
+        public int Team_ID { get ; set; }
+        public int ID { get; set; }
 
         public TeamBooking(int Member_iD, int Team_iD, int iD) 
         {
