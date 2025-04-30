7686829ff2fe58f143b002538da4c2cb44e15595
Malthe Anders Bruhn <mab017@edu.zealand.dk>
2025-04-30 10:32:22 +0200

    Connected Team, TeamBooking, TeamDB and TeamBookingDB to their interfaces.

---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+﻿using Gadevang_Tennis_Klub.Interfaces.Models;
+
+namespace Gadevang_Tennis_Klub.Models
+{
+    public class Team : ITeam
+    {
+        public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ITrainer Trainer { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Capacity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ActiveDay { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Models/TeamBooking.cs
@@ -0,0 +1,11 @@
+﻿using Gadevang_Tennis_Klub.Interfaces.Models;
+
+namespace Gadevang_Tennis_Klub.Models
+{
+    public class TeamBooking : ITeamBooking
+    {
+        public int Member_ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Team_ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
