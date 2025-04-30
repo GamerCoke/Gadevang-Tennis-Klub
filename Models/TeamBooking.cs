f9478154999913456a5a3060c0f1b458667853a4
Malthe Anders Bruhn <mab017@edu.zealand.dk>
2025-04-30 11:13:41 +0200

    Added empty constructors to Team and TeamBooking. Also added Bind Properties.

--- Models/TeamBooking.cs
+++ Models/TeamBooking.cs
@@ -1,13 +1,23 @@
 ﻿using Gadevang_Tennis_Klub.Interfaces.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Gadevang_Tennis_Klub.Models
 {
     public class TeamBooking : ITeamBooking
     {
+        [BindProperty]
         public int Member_ID { get; set; }
+        [BindProperty]
         public int Team_ID { get ; set; }
         public int ID { get; set; }
 
+
+        public TeamBooking()
+        {
+            Member_ID = 0;
+            Team_ID = 0;
+            ID = 0;
+        }
         public TeamBooking(int Member_iD, int Team_iD, int iD) 
         {
             Member_ID = Member_iD;
