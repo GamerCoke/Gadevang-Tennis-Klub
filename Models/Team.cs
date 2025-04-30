37e18386d7422754ea560dc825a62b0833d70472
Malthe Anders Bruhn <mab017@edu.zealand.dk>
2025-04-30 10:43:06 +0200

    Added Constructors to Team and TeamBooking

--- Models/Team.cs
+++ Models/Team.cs
@@ -11,5 +11,16 @@ namespace Gadevang_Tennis_Klub.Models
         public int Capacity { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string ActiveDay { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Team(int iD, string name, string description, ITrainer trainer, int capacity, int price, string activeDay)
+        {
+            ID = iD;
+            Name = name;
+            Description = description;
+            Trainer = trainer;
+            Capacity = capacity;
+            Price = price;
+            ActiveDay = activeDay;
+        }
     }
 }
--- Models/TeamBooking.cs
+++ Models/TeamBooking.cs
@@ -7,5 +7,12 @@ namespace Gadevang_Tennis_Klub.Models
         public int Member_ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int Team_ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public int ID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public TeamBooking(int Member_iD, int Team_iD, int iD) 
+        {
+            Member_ID = Member_iD;
+            Team_ID = Team_iD;
+            ID = iD;
+        }
     }
 }
