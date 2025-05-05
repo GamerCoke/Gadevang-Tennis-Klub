998a98bd3f3ce991f0b630af6aacb14a9054619b
SaraIversen <sara.skovgaard.iversen@gmail.com>
2025-05-05 13:16:05 +0200

    GetAllEvents, CreateEvent, DeleteEvent

--- Pages/Events/GetAllEvents.cshtml.cs
+++ Pages/Events/GetAllEvents.cshtml.cs
@@ -1,3 +1,5 @@
+using Gadevang_Tennis_Klub.Interfaces.Models;
+using Gadevang_Tennis_Klub.Interfaces.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,30 @@ namespace Gadevang_Tennis_Klub.Pages.Events
 {
     public class GetAllEventsModel : PageModel
     {
-        public void OnGet()
+        private IEventDB _eventDB;
+
+        //public string? CurrentUser { get; private set; }
+        public List<IEvent> Events { get; set; }
+
+
+        public GetAllEventsModel(IEventDB eventDB)
         {
+            _eventDB = eventDB;
+        }
+
+        public async Task OnGetAsync()
+        {
+            try
+            {
+                //CurrentUser = HttpContext.Session.GetString("UserName");
+
+                Events = await _eventDB.GetAllEventsAsync();
+            }
+            catch (Exception ex)
+            {
+                Events = new List<IEvent>();
+                ViewData["ErrorMessage"] = ex.Message;
+            }
         }
     }
 }
