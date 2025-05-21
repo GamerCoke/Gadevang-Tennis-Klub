c9850cd6c49e491849205edc405ed8fce490b9d2
Mikkel Barfod <mib010@edu.zealand.dk>
2025-05-21 10:23:56 +0200

    Here comes 2 folders, boy

---
+++ Gadevang Tennis Klub/Services/Secret.cs
@@ -0,0 +1,14 @@
+﻿namespace Gadevang_Tennis_Klub.Services
+{
+    public abstract class Secret
+    {
+        protected static string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=GadevangTennis;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public static string ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+        }
+    }
+}
